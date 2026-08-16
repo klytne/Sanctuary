e40cc2fec3ace34dd5c5c1cf262281a5964170e7
Kelly <tienganekelly@gmail.com>
2026-08-16 19:35:43 +0200

    Add goals and hobby tracker path

--- app.py
+++ app.py
@@ -11,14 +11,16 @@ inject_global_css()
 for key, default in [("logged_in", False), ("username", None), ("user_id", None)]:
     st.session_state.setdefault(key, default)
 
-login_page = st.Page("pages/1_Login.py", title="Login", icon=":material/person:")
 home_page = st.Page("pages/0_Home.py", title="Home", icon=":material/eco:")
+login_page = st.Page("pages/1_Login.py", title="Login", icon=":material/person:")
 journal_page = st.Page("pages/2_Journal.py", title="Journal", icon=":material/edit_note:")
-insights_page = st.Page("pages/3_Insights.py", title="Insights", icon=":material/insights:")
-settings_page = st.Page("pages/4_Settings.py", title="Settings", icon=":material/settings:")
+goals_page = st.Page("pages/3_Goals.py", title="Goals", icon=":material/track_changes:")
+hobby_page = st.Page("pages/4_Hobby_Tracker.py", title="Hobbies", icon=":material/pace:")
+insights_page = st.Page("pages/5_Insights.py", title="Insights", icon=":material/insights:")
+settings_page = st.Page("pages/6_Settings.py", title="Settings", icon=":material/settings:")
 
 if st.session_state.logged_in:
-    pages = [journal_page, insights_page, settings_page]
+    pages = [journal_page, goals_page, hobby_page, insights_page, settings_page]
 else:
     pages = [home_page, login_page]
 
